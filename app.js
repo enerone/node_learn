ff8f3d44753fe72102b8752647128b951aee5d3d
Fabi <enerone@gmail.com>
2021-02-21 20:27:50 -0300

    Initial realease

---
+++ app.js
@@ -0,0 +1,6 @@
+//var Addition = require('./Addition.js');
+//console.log(Addition.AddNumber(23,152));
+
+var localTutor=require('./NodeTutorial.js');
+localTutor.NodeTutorial();
+localTutor.pTutor()
